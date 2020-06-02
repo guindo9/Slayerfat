173bce43266c5e11ff29081b4f868c847442cab6
guindo9 <66290692+guindo9@users.noreply.github.com>
2020-06-01 23:37:42 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace pruevabooster1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button2 = new System.Windows.Forms.Button();
+            this.barratitulo = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnMinimizar = new System.Windows.Forms.PictureBox();
+            this.BtnCerrar = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.Panel2 = new System.Windows.Forms.Panel();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.Button4 = new System.Windows.Forms.Button();
+            this.PictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.Button6 = new System.Windows.Forms.Button();
+            this.Button3 = new System.Windows.Forms.Button();
+            this.Button5 = new System.Windows.Forms.Button();
+            this.Panel1 = new System.Windows.Forms.Panel();
+            this.barratitulo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BtnMinimizar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BtnCerrar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
+            this.Panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.ForeColor = System.Drawing.Color.Transparent;
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // barratitulo
+            // 
+            resources.ApplyResources(this.barratitulo, "barratitulo");
+            this.barratitulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.barratitulo.Controls.Add(this.label1);
+            this.barratitulo.Controls.Add(this.BtnMinimizar);
+            this.barratitulo.Controls.Add(this.BtnCerrar);
+            this.barratitulo.Name = "barratitulo";
+            this.barratitulo.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Barratitulo_MouseDown);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(231)))), ((int)(((byte)(231)))));
+            this.label1.Name = "label1";
+            // 
+            // BtnMinimizar
+            // 
+            resources.ApplyResources(this.BtnMinimizar, "BtnMinimizar");
+            this.BtnMinimizar.Image = global::pruevabooster1.Properties.Resources.minimazar;
+            this.BtnMinimizar.Name = "BtnMinimizar";
+            this.BtnMinimizar.TabStop = false;
+            this.BtnMinimizar.Click += new System.EventHandler(this.BtnMinimizar_Click);
+            // 
+            // BtnCerrar
+            // 
+            resources.ApplyResources(this.BtnCerrar, "BtnCerrar");
+            this.BtnCerrar.Image = global::pruevabooster1.Properties.Resources.cerrar;
+            this.BtnCerrar.Name = "BtnCerrar";
+            this.BtnCerrar.TabStop = false;
+            this.BtnCerrar.Click += new System.EventHandler(this.BtnCerrar_Click);
+            // 
+            // Panel2
+            // 
+            this.Panel2.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.Panel2, "Panel2");
+            this.Panel2.Name = "Panel2";
+            this.Panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Panel2_MouseDown);
+            // 
+            // Button4
+            // 
+            this.Button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.Button4.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.Button4.FlatAppearance.BorderSize = 0;
+            this.Button4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(95)))), ((int)(((byte)(95)))));
+            this.Button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(85)))), ((int)(((byte)(130)))));
+            resources.ApplyResources(this.Button4, "Button4");
+            this.Button4.ForeColor = System.Drawing.Color.LightGray;
+            this.Button4.Name = "Button4";
+            this.Button4.UseVisualStyleBackColor = false;
+            this.Button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // PictureBox1
+            // 
+            resources.ApplyResources(this.PictureBox1, "PictureBox1");
+            this.PictureBox1.Name = "PictureBox1";
+            this.PictureBox1.TabStop = false;
+            this.PictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            this.PictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseClick);
+            this.PictureBox1.MouseLeave += new System.EventHandler(this.PictureBox1_MouseLeave);
+            this.PictureBox1.MouseHover += new System.EventHandler(this.PictureBox1_MouseHover);
+            // 
+            // Button1
+            // 
+            this.Button1.BackColor = System.Drawing.Color.Transparent;
+            this.Button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.Button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.Button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.Button1, "Button1");
+            this.Button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(164)))), ((int)(((byte)(213)))));
+            this.Button1.Name = "Button1";
+            this.Button1.UseVisualStyleBackColor = false;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Button6
+            // 
+            this.Button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(47)))), ((int)(((byte)(47)))));
+            this.Button6.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(164)))), ((int)(((byte)(213)))));
+            this.Button6.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(95)))), ((int)(((byte)(95)))));
+            this.Button6.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(99)))), ((int)(((byte)(151)))));
+            resources.ApplyResources(this.Button6, "Button6");
+            this.Button6.ForeColor = System.Drawing.Color.White;
+            this.Button6.Name = "Button6";
+            this.Button6.UseVisualStyleBackColor = false;
+            this.Button6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // Button3
+            // 
+            this.Button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(36)))), ((int)(((byte)(36)))));
+            this.Button3.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(54)))), ((int)(((byte)(54)))));
+            this.Button3.FlatAppearance.BorderSize = 0;
+            this.Button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(95)))), ((int)(((byte)(95)))));
+            this.Button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(99)))), ((int)(((byte)(151)))));
+            resources.ApplyResources(this.Button3, "Button3");
+            this.Button3.ForeColor = System.Drawing.Color.White;
+            this.Button3.Name = "Button3";
+            this.Button3.UseVisualStyleBackColor = false;
+            this.Button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Button5
+            // 
+            this.Button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(36)))), ((int)(((byte)(36)))));
+            this.Button5.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.Button5.FlatAppearance.BorderSize = 0;
+            this.Button5.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(95)))), ((int)(((byte)(95)))));
+            this.Button5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(99)))), ((int)(((byte)(151)))));
+            resources.ApplyResources(this.Button5, "Button5");
+            this.Button5.ForeColor = System.Drawing.Color.Transparent;
+            this.Button5.Name = "Button5";
+            this.Button5.UseVisualStyleBackColor = false;
+            this.Button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // Panel1
+            // 
+            this.Panel1.BackColor = System.Drawing.Color.Transparent;
+            this.Panel1.Controls.Add(this.Button5);
+            this.Panel1.Controls.Add(this.Button3);
+            resources.ApplyResources(this.Panel1, "Panel1");
+            this.Panel1.Name = "Panel1";
+            this.Panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Panel1_MouseDown);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.Controls.Add(this.Button6);
+            this.Controls.Add(this.barratitulo);
+            this.Controls.Add(this.Button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Panel1);
+            this.Controls.Add(this.Panel2);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.PictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.barratitulo.ResumeLayout(false);
+            this.barratitulo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BtnMinimizar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BtnCerrar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
+            this.Panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel barratitulo;
+        private System.Windows.Forms.PictureBox BtnCerrar;
+        private System.Windows.Forms.PictureBox BtnMinimizar;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Panel Panel2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button Button4;
+        private System.Windows.Forms.PictureBox PictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.Button Button6;
+        private System.Windows.Forms.Button Button3;
+        private System.Windows.Forms.Button Button5;
+        private System.Windows.Forms.Panel Panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.DirectoryServices;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace pruevabooster1
+{
+    public partial class Form1 : Form
+    {
+        public string InitialDirectory { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+            //Activar boost 
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            button2.Show();
+            Button1.Hide();
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = @"/C TASKKILL /IM explorer.exe /F & net stop wuauserv & net stop SENS & net stop CryptSvc & net stop Winmgmt & net stop Themes & net stop SysMain & net stop stisvc & net stop FontCache & net stop WbioSrvc & net stop WSearch & net stop EventSystem & net stop DeviceAssociationService & net stop DeviceAssociationService & net stop Dnscache & net stop TimeBroker & net stop VaultSvc & net stop UsoSvc & net stop wmiApSrv & net stop SgrmBroker & net stop Spooler & net stop wscsvc & net stop RmSvc & net stop AppXSvs & net stop InstallService & net stop DisplayEnhancementService & net stop TabletInputService & net stop fsvc & net stop SmsRouter & net stop SDRSVC & net stop NVDisplay.ContainerLocalSystem & net stop AppXSvc & cd C:\Program Files\Game_Booster\Archivos & EmptyStandbyList.exe workingsets & EmptyStandbyList.exe modifiedpagelist & EmptyStandbyList.exe priority0standbylist & EmptyStandbyList.exe standbylis & exit";
+            process.StartInfo = startInfo;
+            process.Start();
+            OpenFileDialog buscar = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\Program Files\Game_Booster\Juegos",
+                Title = "Game Booster",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "url",
+                Filter = "Todos los juegos (*.*)|*.*|Ver mas juegos (*.url)|*.url",
+                FilterIndex = 2,             
+            };
+
+            if (buscar.ShowDialog() == DialogResult.OK)
+            {
+                Process.Start(buscar.FileName);
+            }
+        }
+        //Restaurar Windows
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            Button1.Show();
+            button2.Hide();
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = @"/C cd C:\Windows & start explorer.exe & net start wuauserv & net start SENS & net start CryptSvc & net start Winmgmt & net start Themes & net start SysMain & net start stisvc & net start FontCache & net start WbioSrvc & net start WSearch & net start EventSystem & net start DeviceAssociationService & net start DeviceAssociationService & net start Dnscache & net start TimeBroker & net start VaultSvc & net start UsoSvc & net start wmiApSrv & net start SgrmBroker & net start Spooler & net start wscsvc & net start RmSvc & net start AppXSvs & net start InstallService & net start DisplayEnhancementService & net start TabletInputService & net start fsvc & net start SmsRouter & net start SDRSVC & net start NVDisplay.ContainerLocalSystem & net start AppXSvc & cd C:\Program Files\Game_Booster\Archivos & EmptyStandbyList.exe workingsets & EmptyStandbyList.exe modifiedpagelist & EmptyStandbyList.exe priority0standbylist & EmptyStandbyList.exe standbylis & exit";
+            process.StartInfo = startInfo;
+            process.Start();
+
+        }
+        //Agregar juegos
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            Process.Start("explorer.exe", @"C:\Program Files\Game_Booster\Juegos");
+        }
+         //Liberar Ram
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = @"/C cd C:\Program Files\Game_Booster\Archivos & EmptyStandbyList.exe workingsets & EmptyStandbyList.exe modifiedpagelist & EmptyStandbyList.exe priority0standbylist & EmptyStandbyList.exe standbylist";
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+         //Abrir juego o aplicacion
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog buscar = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\Program Files\Game_Booster\Juegos",
+                Title = "Game Booster",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "url",
+                Filter = "Todos los juegos (*.*)|*.*|Ver mas juegos (*.url)|*.url",
+                FilterIndex = 2,
+            };
+
+            if (buscar.ShowDialog() == DialogResult.OK)
+            {
+                Process.Start(buscar.FileName);
+            }
+        }
+
+        //Barra de titulo y botones
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        private void Barratitulo_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+          
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+        
+        private void BtnCerrar_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"C:\Program Files\Game_Booster\Scrips\cerrar.vbs");
+            Application.Exit();
+        }
+        private void BtnMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+         //Paneles de botones
+        private void Panel2_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void Panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+         //Propiedades de boton para imagen de restaurar
+        private void PictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+           
+        }
+
+        private void PictureBox1_MouseHover(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+
+        }
+          //Limpiar archivos temporales
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = @"/C RD /S /Q %temp% & md %temp% & md %temp% & rd C:\windows\TEMP /s /q & md C:\windows\TEMP & md C:\windows\TEMP";
+            process.StartInfo = startInfo;
+            process.Start();
+
+        }
+        
+        
+    }
+}
+       
+
+        
+    
+
